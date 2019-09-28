436fbf54d26d07835d4d56072a7a5f26bd8e6110
Victor Oliveira <victormegale96@yahoo.com.br>
2019-09-28 18:26:08 -0300

    'comitado'

--- source/src/test/TesteConsulta.java
+++ source/src/test/TesteConsulta.java
@@ -3,23 +3,23 @@ package test;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import dao.ConsultaDAO;
-import model.Consulta;
+import dao.HorarioDAO;
+import model.Horario;
 
 public class TesteConsulta {
-	private ConsultaDAO pessoaDAO;
-	private Consulta pessoa;
+	private HorarioDAO pessoaDAO;
+	private Horario pessoa;
 
 	@Test
 	public void testSalvar() {
-		pessoa = new Consulta();
+		pessoa = new Horario();
 
 		pessoa.setId(1);
 		pessoa.setNome("Teste Inserção Cliente");
 		pessoa.setEmail("pessoa@teste.com");
 		pessoa.setTelefone("4599991231");
 
-		pessoaDAO = new ConsultaDAO();
+		pessoaDAO = new HorarioDAO();
 		pessoaDAO.salvar(pessoa);
 
 		assertTrue(pessoaDAO.listarTodos().size() > 0);
